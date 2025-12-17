6fb84632da5abaa776b7ea6692e5dcd52f7f5583
Lucas Silveira <silveira.codes@gmail.com>
2025-12-17 19:24:12 -0300

    adding zeitwerk initializer

--- config/application.rb
+++ config/application.rb
@@ -13,31 +13,6 @@ module Soat4PaymentService
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.autoload_paths += %W[
-      #{config.root}/app/domain
-      #{config.root}/app/domain/ports
-      #{config.root}/app/domain/product
-      #{config.root}/app/domain/client
-      #{config.root}/app/domain/cart
-      #{config.root}/app/domain/payment
-      #{config.root}/app/domain/use_cases
-      #{config.root}/app/domain/use_cases/product
-      #{config.root}/app/domain/use_cases/client
-      #{config.root}/app/domain/use_cases/cart
-      #{config.root}/app/domain/use_cases/payment
-      #{config.root}/app/infrastructure
-      #{config.root}/app/infrastructure/persistence
-      #{config.root}/app/infrastructure/external_apis
-      #{config.root}/app/infrastructure/external_apis/mercado_pago
-      #{config.root}/app/infrastructure/persistence/active_record
-      #{config.root}/app/infrastructure/persistence/active_record/products
-      #{config.root}/app/infrastructure/persistence/active_record/clients
-      #{config.root}/app/infrastructure/persistence/active_record/carts
-      #{config.root}/app/infrastructure/persistence/mongo
-      #{config.root}/app/infrastructure/messaging
-      #{config.root}/app/infrastructure/workers
-    ]
-
     config.hosts << 'pagamento-service'
     config.hosts << /.*\.ngrok-free\.dev/
     # Configuration for the application, engines, and railties goes here.
