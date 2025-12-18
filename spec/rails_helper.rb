b1e583c1d41683e938e78348360c401a1b297c35
Lucas Silveira <silveira.codes@gmail.com>
2025-12-17 22:18:21 -0300

    mocking mercado pago envs for ci/cd

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,5 +22,14 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.before(:each) do
     allow(Bunny).to receive(:new).and_return(instance_double(Bunny::Session, start: nil, create_channel: instance_double(Bunny::Channel, fanout: nil, queue: nil)))
+
+
+    stub_const('ENV', ENV.to_hash.merge(
+      'MERCADOPAGO_TOKEN' => 'test_token',
+      'MERCADOPAGO_USER_ID' => 'test_user_id',
+      'MERCADOPAGO_EXTERNAL_POS_ID' => 'test_pos_id',
+      'MERCADOPAGO_NOTIFICATION_URL' => 'http://test.url',
+      'MERCADOPAGO_SECRET' => 'test_secret'
+    ))
   end
 end
